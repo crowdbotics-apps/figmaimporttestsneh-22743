66aeeff7ed689e7002d435c723bffbc52b98d041
Crowdbotics <team@crowdbotics.com>
2021-07-12 14:46:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/figmaimporttestsneh_22743/MainActivity.java
@@ -0,0 +1,15 @@
+package com.figmaimporttestsneh_22743;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "figmaimporttestsneh_22743";
+  }
+}
